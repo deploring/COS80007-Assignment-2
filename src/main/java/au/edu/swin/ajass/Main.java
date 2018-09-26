c7be22784dfa8bcb76ef31cbf43ce606fa1c6eca
Joshua <josh@rpg.solar>
2018-09-26 17:33:07 +1000

    important

--- src/main/java/au/edu/swin/ajass/Main.java
+++ src/main/java/au/edu/swin/ajass/Main.java
@@ -1,11 +1,13 @@
 package au.edu.swin.ajass;
 
+import javax.swing.*;
+
 /**
  *yo bro
  */
 public class Main {
 
     public static void main(String[] args) {
-
+        JOptionPane.showMessageDialog(null, "fuck");
     }
 }
